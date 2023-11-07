274726cc8aab94732f07578c5552d4c6aed1aede
DALMTONI <144811026+DALMTONI@users.noreply.github.com>
2023-11-07 09:22:54 +0000

    exo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <string>
+#include "Personnage.hpp" 
 
 using namespace std;
 
+#include <iostream>
+#include "Personnage.hpp" //Ne pas oublier
+
+using namespace std;
+
+int main()
+{
+    Personnage david, goliath;
+    //Création de 2 objets de type Personnage : david et goliath
+
+    goliath.attaquer(david); //goliath attaque david
+    david.boirePotionDeVie(20); //david récupère 20 de vie en buvant une potion
+    goliath.attaquer(david); //goliath attaque david
+    david.attaquer(goliath); //david contre-attaque... c'est assez clair non ? 
+    goliath.changerArme("Double hache tranchante veneneuse de la mort", 40);
+    goliath.attaquer(david);
+
+    return 0;
+}
+
+/*
 int main() {
     cout << "Hello World\n";
     cout << "Input: ";
-    string data;
-    getline(cin, data);
+    int data;
+    cin >> data;
+    data = data + 2;
     cout << "Output: " << data << "\n\n";
     return 0;
 }
+*/
